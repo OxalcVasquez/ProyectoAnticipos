9c9807d459c156497d13f79373bf07e62256c613
RodrigoRuidi <rruidias10@gmail.com>
2022-11-12 20:50:04 -0500

    Se cambió la BASE_URL

--- app/src/main/java/com/usat/desarrollo/moviles/appanticipos/data/remote/api/ApiAdapter.java
+++ app/src/main/java/com/usat/desarrollo/moviles/appanticipos/data/remote/api/ApiAdapter.java
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ApiAdapter {
     private static ApiService apiService;
 
-    private static final String BASE_URL = "http://192.168.1.2:3001/";
+    private static final String BASE_URL = "https://web-services-anticipos.onrender.com/";
 
     public static ApiService getApiService() {
         if (apiService == null) {
